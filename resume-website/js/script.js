47a189214104f19217e89f565f30b0821f02224c
achrafmagouh <mo.magouh@gmail.com>
2020-05-05 01:45:48 -0400

    Validation message
    Add validation succes message when we click on submit button

--- resume-website/js/script.js
+++ resume-website/js/script.js
@@ -3,6 +3,8 @@ var inputMail = document.getElementById('input-mail');
 var inputPhone = document.getElementById('input-phone');
 var inputMessage = document.getElementById('input-message');
 var submitButton = document.querySelector('.submit-button');
+var submitForm = document.getElementById('submit-form');
+
 var regEx = /\S+@\S+\.\S+/;
 var form = document.querySelector('.contact-form')
 
@@ -18,10 +20,54 @@ function checkInput() {
 
 
 
-
-
-
 form.addEventListener('submit', function(event){
     event.preventDefault();
 })
 
+
+//$(document).ready(function(){
+//    $('#submit-form').bind('click', function (event){
+//        if (event.type == "click")
+//        $('#submit-result').html('Button Clicked');
+//    } else {
+//            $(this).removeClass('alert alert-success');
+//                           })
+//})
+
+
+    $(document).ready (function() {
+        $('#submit-form').click(function (){
+            $('#submit-result').show('fade');
+            
+            setTimeout(function () {
+                $('#submit-result').hide('fade');
+        }, 2000);
+        });
+
+        $('#linkClose').click(function () {
+            $('#submit-result').hide('fade');
+
+    });
+ });
+
+
+
+
+
+
+
+
+//
+//
+//$(document).ready(function() {
+//  $("form").submit(function(event){
+//    event.preventDefault();
+//    inputName = $("#input-name").val();
+//    inputMail = $("#input-mail").val();
+//    inputPhone  = $("#input-phone").val();
+//    inputMessage = $("#input-message").val();
+//    submitButton = $("#input-message").val();
+//      $(".contact-form")
+//    
+//});
+//                  });
